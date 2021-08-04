28b7d127fbd401df185d3b6e742deab8cd46f7ae
DavidBekkers <42903287+Bowowzahoya@users.noreply.github.com>
2021-08-04 18:31:30 +0800

    2.0.0
    Starting on complete redo of module. Renamed all constants.

--- src/lens_analysis/families.py
+++ src/lens_analysis/families.py
@@ -20,62 +20,67 @@ from .constants import *
 from .utilities import join, join_set, join_max, join_sum, join_earliest 
 from .utilities import join_size, join_cols, join_most
 
-FUNC_D = {IPCR_COL:(IPCR_COL, join_set),
-          FAM_SZ_COL:(SET_FAM_SZ_COL,join_max),
-          CIT_COL:(TOT_CIT_COL, join_sum),
-          JUR_COL:(JURS_COL, join_set),
-          KIND_COL:(KINDS_COL, join_set),
-          PDAT_COL:(ERL_PDAT_COL, join_earliest),
-          PY_COL:(ERL_PY_COL, join_earliest),
-          APP_NUM_COL:(APP_NUMS_COL, join_set),
-          APP_COL:(APP_COL, join_set),
-          INV_COL:(INV_COL, join_set),
-          OWN_COL:(OWN_COL, join_set),
-          TYP_COL:(TYPS_COL, join_set),
-          REF_COL:(TOT_REF_COL, join_sum),
-          EXT_FAM_SZ_COL:(SET_EXT_FAM_SZ_COL, join_max),
-          NPL_COL:(NPL_COL,  join_sum),
-          NPL_RES_COL :(NPL_RES_COL , join_sum),
-          IX_COL:(RES_FAM_SZ_COL, join_size),
-          PNUM_COL:(PNUMS_COL, join_set),
-          LID_COL:(LIDS_COL, join_set),
-          ANUM_COL:(ANUM_COL, join_set),
-          ADAT_COL:(ERL_ADAT_COL, join_earliest),
-          TIT_COL:(TIT_COL, join_set)}
+DEFAULT_CONVERSION_FUNCTION_DICT = {\
+JURISDICTION_COL:(JURISDICTIONS_COL, join_set),
+KINDS_COL:(KINDS_COL, join_set),
+PUBLICATION_NUMBER_COL:(PUBLICATION_NUMBERS_COL, join_set),
+LENS_ID_COL:(LENS_IDS_COL, join_set),
+PUBLICATION_DATE_COL:(EARLIEST_PUBLICATION_DATE_COL, join_earliest),
+PUBLICATION_YEAR_COL:(EARLIEST_PUBLICATION_YEAR_COL, join_earliest),
+APPLICATION_NUMBER_COL:(APPLICATION_NUMBERS_COL, join_set),
+APPLICATION_DATE_COL:(EARLIEST_APPLICATION_DATE_COL, join_earliest),
+EARLIEST_PRIORITY_DATE_COL:(EARLIEST_PRIORITY_DATE_COL, join_earliest),
+TITLE_COL:(TITLE_COL, join_set),
+ABSTRACT_COL:(FIRST_ABSTRACT_COL, join_first),
+APPLICANTS_COL:(FAMILY_APPLICANTS_COL, join_set),
+INVENTORS_COL:(INVENTORS_COL, join_set),
+OWNERS_COL:(OWNERS_COL, join_set),
+URL_COL:(URLS_COL, join_set),
+DOCUMENT_TYPE_COL:(DOCUMENT_TYPES_COL, join_set),
+CITES_PATENT_COUNT_COL:(FAMILY_CITES_PATENT_COUNT_COL, join_sum),
+CITED_PATENT_COUNT_COL:(FAMILY_CITED_PATENT_COUNT_COL, join_sum),
+SIMPLE_FAMILY_SIZE_COL:(SIMPLE_FAMILY_SIZE_COL, join_max),
+EXTENDED_FAMILY_SIZE_COL:(EXTENDED_FAMILY_SIZE_COL, join_max),
+CPC_CLASSIFICATIONS_COL:(CPC_CLASSIFICATIONS_COL, join_sum),
+IPCR_CLASSIFICATIONS_COL:(IPCR_CLASSIFICATIONS_COL, join_set),
+US_CLASSIFICATIONS_COL:(US_CLASSIFICATIONS_COL, join_set),
+CITED_NPL_COUNT_COL:(CITED_NPL_COUNT_COL, join_sum),
+RESOLVED_CITED_NPL_COUNT_COL:(RESOLVED_CITED_NPL_COUNT_COL, join_sum),
+RESOLVED_CITED_NPL_LENS_IDS_COL:(RESOLVED_CITED_NPL_LENS_IDS_COL, join_set),
+RESOLVED_CITED_NPL_EXTERNAL_IDS_COL:(RESOLVED_CITED_NPL_EXTERNAL_IDS_COL, join_set),
+NPL_CITATIONS_COL:(NPL_CITATIONS_COL, join_set),
+HAS_FULL_TEXT_COL:(INCLUDED_SIMPLE_FAMILY_SIZE_COL, join_size)}
 
 
-def _sort_prios(prio):
+def _sort_priority_numbers(priority_numbers):
     # Needed because otherwise simple families might be
-    # missed (NL00918;NL988 should be same family as NL988;NL00918)
-    return SEP.join(sorted(prio.split(SEP)))
+    # missed (NL00918;;NL988 should be same family as NL988;;NL00918)
+    return SEP.join(sorted(priority_numbers.split(SEP)))
 
-def merge_to_family(df, func_d_custom={}):
+def merge_to_family(lens_export, custom_conversion_function_dict={}):
     """
-    Merges patent publications into families.
+    Merges a Lens patent export at the publication level into families.
     
     Parameters:
-        df: DataFrame with publication per row (index Lens index)
-        This is directly the .csv export from Lens
-        Has columns at least those that are included 
-        in FUNC_D and func_d_custom
+        lens_export: DataFrame
+        Lens export with publications per row
         
-        func_d_custom: dict, with tuples of length 2 as values, optional
-        A dictionary of which columns to map to which columns
-        using which function. Uses same structure as FUNC_D
+        custom_conversion_function_dict: dict with tuples of length 2 as values, optional
+        A dictionary of which columns in the lens_export to map to which columns
+        in the family DataFrame, using which function. Uses same structure as DEFAULT_CONVERSION_FUNCTION_DICT
         and will override if there is overlap. The default is {}.
     
     Returns:
-        fam: DataFrame of patent families with index sorted priority number
+        families: DataFrame of patent families with as index the sorted priority numbers
     """
-    df[SRT_PRIO_COL] = df[PRIO_COL].apply(_sort_prios)
+    lens_export[SORTED_PRIORITY_NUMBERS_COL] = lens_export[PRIORITY_NUMBERS_COL].apply(_sort_priority_numbers)
     
-    groupby = df.groupby(SRT_PRIO_COL)
+    groupby = lens_export.groupby(SORTED_PRIORITY_NUMBERS_COL)
     
-    # update function dictionary
-    func_d = FUNC_D.copy()
-    for key in func_d_custom:
-        func_d[key] = func_d_custom[key]
+    conversion_function_dict = DEFAULT_CONVERSION_FUNCTION_DICT.copy()
+    for key in custom_conversion_function_dict:
+        conversion_function_dict[key] = custom_conversion_function_dict[key]
     
-    fam = groupby.apply(join_cols, func_d)
-    return fam
+    families = groupby.apply(join_cols, conversion_function_dict)
+    return families
     
