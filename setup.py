504d3e907cb29958b57712e4bd45f0410bb9bdd0
DavidBekkers <42903287+Bowowzahoya@users.noreply.github.com>
2022-01-04 13:31:21 +0800

    2.3.11.5
    Fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     package_dir={"":"src"},
     url='https://github.com/Bowowzahoya/lens_analysis',
     description='Analysis of Lens patent exports',
-    long_description=open('README.txt').read(),
+    long_description=open('README.txt', encoding="utf-8").read(),
     install_requires=[
         "pandas",
         ],
