38e6386d617396cd596fc954b4e2c539fbe78533
DavidBekkers <42903287+Bowowzahoya@users.noreply.github.com>
2022-06-24 14:22:10 +0800

    2.5.1
    Cleaned up package, updated README and example.

---
+++ README.md
@@ -0,0 +1,60 @@
+# lens_analysis
+A module for doing analytics on downloaded Lens patent exports
+
+## Background
+Lens offers some analytics and statistics on the website, but sometimes too limited for advanced analyses. For example, only the top 100 applicants are listed, most information is on publication level, not on family level, it is not possible to add different version of the same applicant together, and there are no citation scores or market coverage scores.
+
+This module provides some of these basic metrics for analysis on applicant level. On top of above functionality, it also adds some functionality for grouping applicants together per type
+e.g. Chinese companies, Chinese academia, American companies, EU-27 companies, et cetera. The applicant types are now according to two axes: EU-27, Chinese, American, Rest of World on one axis, and Company, Academia, Individual, Other or Unknown on the other axis. It is possible to create you own grouping based on a collection of "dataframe compressors".
+
+## Usage
+Normal usage would be to first aggregate an export to patent families, then aggregate to applicants (possibly with aliases for applicants that exist under different names/spelling), then possibly aggregate even further to count patents for particular applicant types.
+
+### aggregate_to_family()
+Will group patent families together from a lens export (in pandas DataFrame format). 
+
+```
+import pandas as pd
+lens_export = pd.read_csv("lens-export.csv", index_col=0)
+families = aggregate_to_family(lens_export)
+```
+### add_extra_family_information()
+Will add extra columns with additional family information (such as citation scores, market coverage, etc.)
+
+```
+families = add_extra_family_information(families)
+```
+### aggregate_to_applicants()
+Will group patent applicants together from a patent families DataFrame. Can possibly include aliases for different version of the same applicant (e.g. IBM UK, IBM CO LTD, 国际商机公司, et cetera). 
+
+Note that ou need to first use add_extra_family_information() on the families DataFrame to also get citation information in the resultant applicant DataFrame.
+
+```
+aliases = pd.read_excel("aliases.xlsx", index_col=0, squeeze=True)
+applicants = aggregate_to_applicants(families, aliases=aliases)
+```
+
+### guess_aliases()
+Will guess aliases for a number of applicants, based on joint patents, and shortened versions of the applicant name.
+
+Please note that inventors that appear in tandem with the applicant will also appear in this list, as well as any collaboration partners with whom joint patents have been filed. You have to get rid of these yourself.
+
+```
+# to guess aliases for top 20 applicants
+aliases = guess_aliases(applicants, applicants.index[0:20])
+aliases.to_excel("guessed_aliases.xlsx")
+```
+### add_labels()
+Will classify patent applicants and give them a label according to applicant type.
+
+Classification is based on a list of known applicants; keywords to indicate university, company; company types per country; applicant in inventor list (indicates individual); main priority jurisdiction.
+
+```
+applicants = add_labels(applicants)
+```
+### aggregate_to_applicant_types()
+Will group patent applicants together from a patent applicants DataFrame. The patent applicants DataFrame will need to have labels added first using the function add_labels()
+
+```
+applicant_types = aggregate_to_applicant_types(applicants)
+```
--- examples/applicant_types_per_jurisdiction.py
+++ examples/applicant_types_per_jurisdiction.py
@@ -1,22 +1,30 @@
+"""
+This script imports a lens export on patents dealing with invasive species 
+(a niche area with inventions contributing to biodiversty solutions).
+It first merges the exports to patent families, then families to applicants,
+then applicants to applicant types. 
+If interested in manually getting rid of aliases for a few top applicants,
+uncomment the middle section.
+"""
 import os
+import pandas as pd
+from datetime import datetime
+
+# following 5 lines not necessary if package installed on system
 import sys
-
-
 package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 
                                             '../src'))
 if package_path not in sys.path:
     sys.path.insert(0, package_path)
 
+import lens_analysis as la
+
 RESOURCES_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)),"res/")
 OUTPUT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)),"out/")
 
-import lens_analysis as la
-import pandas as pd
-from datetime import datetime
-
 lens_export = pd.read_csv(RESOURCES_FOLDER+"invasive-species.csv", index_col=0)
 
-# First get patent families from Lens Export
+# First get patent families from the Lens export
 print(f"{datetime.now().time()}: Merging to families.")
 families = la.aggregate_to_family(lens_export)
 print(f"{datetime.now().time()}: Adding extra family information.")
@@ -24,6 +32,7 @@ families = la.add_extra_family_information(families)
 print(f"{datetime.now().time()}: Exporting families.")
 families.to_excel(OUTPUT_FOLDER+"families.xlsx")
 
+# Split per jurisdiction
 print(f"{datetime.now().time()}: Splitting per jurisdiction and exporting.")
 chinese_jurisdiction_mask = families["Jurisdictions"].str.contains("CN")
 families_in_china = families.loc[chinese_jurisdiction_mask]
@@ -41,16 +50,18 @@ print(f"{datetime.now().time()}: Merging to applicants.")
 applicants = la.aggregate_to_applicants(families)
 applicants.to_excel(OUTPUT_FOLDER+"applicants.xlsx")
 
-print(f"{datetime.now().time()}: Guessing aliases of top 20.")
-aliases = la.guess_aliases(applicants, applicants.index[0:20])
-aliases.to_excel(OUTPUT_FOLDER+"aliases.xlsx")
-input("Adapt the aliases file, save as 'aliases_adapted.xlsx' and press enter to continue...")
+# If interested in manually getting rid of aliases for the same company, uncomment:
+# print(f"{datetime.now().time()}: Guessing aliases of top 20.")
+# aliases = la.guess_aliases(applicants, applicants.index[0:20])
+# aliases.to_excel(OUTPUT_FOLDER+"aliases.xlsx")
+# input("Adapt the aliases file, save as 'aliases_adapted.xlsx' and press enter to continue...")
 
-print(f"{datetime.now().time()}: Remerging applicants with aliases included.")
-aliases_adapted = pd.read_excel(OUTPUT_FOLDER+"aliases_adapted.xlsx", index_col=0, squeeze=True)
-applicants_aliased = la.aggregate_to_applicants(families, aliases=aliases_adapted)
-applicants_aliased.to_excel(OUTPUT_FOLDER+"applicants_aliased.xlsx")
+# print(f"{datetime.now().time()}: Remerging applicants with aliases included.")
+# aliases_adapted = pd.read_excel(OUTPUT_FOLDER+"aliases_adapted.xlsx", index_col=0, squeeze=True)
+# applicants_aliased = la.aggregate_to_applicants(families, aliases=aliases_adapted)
+# applicants_aliased.to_excel(OUTPUT_FOLDER+"applicants_aliased.xlsx")
 
+# Merge families to applicants per jurisdiction
 print(f"{datetime.now().time()}: Merging to applicants China.")
 applicants_in_china = la.aggregate_to_applicants(families_in_china)
 print(f"{datetime.now().time()}: Labeling applicants China.")
