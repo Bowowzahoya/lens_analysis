a3a04a88b2400e44431591fbcab27ed45c86a3b6
DavidBekkers <42903287+Bowowzahoya@users.noreply.github.com>
2022-09-18 11:50:16 +0800

    2.6.1
    Added TW labeler and EU-US-China labeler to top level imports.

--- src/lens_analysis/__init__.py
+++ src/lens_analysis/__init__.py
@@ -7,6 +7,6 @@ Created on Mon Mar  1 13:17:35 2021
 
 from .applicants import aggregate_to_applicants
 from .families import aggregate_to_family, add_extra_family_information
-from .applicant_labeler import aggregate_to_applicant_types, add_labels, NL_LABELER
+from .applicant_labeler import aggregate_to_applicant_types, add_labels, NL_LABELER, TW_LABELER, EU_US_CHINA_LABELER
 from .applicant_disambiguation import guess_aliases
 from .constants import CN_JURISDICTION, US_JURISDICTION, EU_JURISDICTIONS, REST_OF_WORLD_JURISDICTIONS
